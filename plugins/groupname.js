76f1633d90f3be1ea374220a0f0d11e0401ed837
nabildzr <65263479+nabildzr@users.noreply.github.com>
2024-03-16 12:15:40 +0700

    Delete plugins directory

--- plugins/groupname.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, participants, groupMetadata }) => {
-conn.reply(m.chat, `${groupMetadata.subject}`, m)
-}
-handler.help = ['ceknamegc']
-handler.tags = ['group']
-handler.command = /^(cekname(group|gc))$/i
-handler.group = true
-
-module.exports = handler
