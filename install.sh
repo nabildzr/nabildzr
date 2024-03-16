58f11be42630234b0b94de0f69a9e843cd8c9f81
nabildzr <65263479+nabildzr@users.noreply.github.com>
2024-03-16 12:17:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,20 +20,4 @@ pkg install ffmpeg
 pkg install imagemagick
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 yarn
-
-echo ":::::::::::::::::::::::::::::::::"
-echo ":::::::::::::::::::::::::::::::::"
-echo ":::::         Dann          :::::"
-echo ":::::                       :::::"
-echo ":::::   :::::      :::::    :::::"
-echo ":::::   :::::      :::::    :::::"
-echo ":::::                       :::::"
-echo ":::::     ::::::::::::      :::::"
-echo ":::::     ::::::::::::      :::::"
-echo ":::::                       :::::"
-echo ":::::::::::::::::::::::::::::::::"
-echo ":::::::::::::::::::::::::::::::::"
-echo "[*] JANGAN DISHARE, DI SHARE REPO INI AKAN DI HAPUS !!"
-echo "[*] Made by Muhammad Ridwan Reynaldy"
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
 npm start
